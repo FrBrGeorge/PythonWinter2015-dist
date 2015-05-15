680f5a80c7254c4d3edae06de9cfeb4f269248d5
George V. Kouryachy (Fr. Br. George) <george@altlinux.ru>
2015-05-15 16:26:20 +0300

    find-packages

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
-from setuptools import setup
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
 
 setup(name = 'thepackage',
       version = '0.1',
       description = 'Dummy package',
+      long_description = open(os.path.join(os.path.dirname(__file__), "README.md")).read(),
       url = 'https://github.com/FrBrGeorge/PythonWinter2015-dist',
       author = 'Fr. Br. George',
       author_email = 'frbrgeorge@gmail.com',
       license = 'BSD',
       keywords = "dummy educational",
-      packages = ['thepackage'],
+      packages = find_packages(),
       zip_safe = False,
       classifiers=[
         "Development Status :: 3 - Alpha",
